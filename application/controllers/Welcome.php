0d7c67634f86b957b10234e792eaf6546cf48098
Ardianta Pargo <ardianta_pargo@yahoo.co.id>
2017-02-20 15:16:03 +0800

    hapus lorem

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -36,5 +36,7 @@ class Welcome extends CI_Controller {
 		$this->pdf->setPaper('A4', 'potrait');
 		$this->pdf->filename = "laporan-petanikode.pdf";
 		$this->pdf->load_view('laporan_pdf', $data);
+
+
 	}
 }
